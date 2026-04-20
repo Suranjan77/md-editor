00ca4bea390b7e6f834d6e728c0199d22975200b
suranjan77 <7suranjanpoudel7@gmail.com>
2026-04-20 15:35:00 +0100

    fix issue with titlebar decoration in linux

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,14 @@
 )]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        // Force the app to use X11/XWayland instead of native Wayland
+        std::env::set_var("GDK_BACKEND", "x11");
+
+        // std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    }
+
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
